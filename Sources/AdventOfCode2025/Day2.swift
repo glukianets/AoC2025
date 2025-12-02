fe4405f51e9660c710f0499ff9cac8eed7321e20
glukianets <glukianets@users.noreply.github.com>
2025-12-02 17:49:42 +0100

    Day 2 Optimized

--- Sources/AdventOfCode2025/Day2.swift
+++ Sources/AdventOfCode2025/Day2.swift
@@ -36,15 +36,39 @@ class Day2A: DayCommand {
 
 class Day2B: Day2A {
     override func isInvalid(_ id: Int) -> Bool {
+        isInvalidOptimal(id)
+    }
+    
+    func isInvalidIdiomatic(_ id: Int) -> Bool {
         guard id > 9 else { return false }
 
         let digits = Array(id.digits)
-        let candidateLengths = (1...digits.count/2).lazy.filter { digits.count % $0 == 0 }.reversed()
-        return candidateLengths.contains { length in digits.chunks(ofCount: length).adjacentPairs().allSatisfy(==) }
+        let lengths = (1...digits.count/2).lazy.filter { digits.count % $0 == 0 }
+        return lengths.contains { digits.chunks(ofCount: $0).lazy.adjacentPairs().allSatisfy(==) }
+    }
+
+    func isInvalidOptimal(_ id: Int) -> Bool {
+        let digits = Array(id.digits)
+        
+        outer: for index in digits.indices.dropFirst().prefix(digits.count / 2) {
+            let distance = digits.distance(from: digits.startIndex, to: index)
+            guard digits.count % distance == 0 else { continue outer }
+            
+            for i in 1 ..< digits.count / distance {
+                let range = index.advanced(by: distance * (i - 1)) ..< index.advanced(by: distance * i)
+                guard digits[range] == digits[..<index] else { continue outer }
+            }
+            return true
+        }
+        return false
     }
 }
 
 private extension BinaryInteger {
+    var digitCount: Int {
+        Int(floor(log10(2) * Float(self.bitWidth)) + 1)
+    }
+    
     var digits: some Sequence<Self> {
         sequence(state: self) { value in
             guard value != 0 else { return nil }
